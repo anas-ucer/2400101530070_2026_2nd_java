b696dfe7d5ae31841880ee4ae45575c63720885c
anas-ucer <mohdanas22794@gmail.com>
2026-02-24 07:36:31 +0000

    changes

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+    <h1>MY NAME MOHAMMAD ANAS{employe()}</h1>
+    <h1>ucer</h1>
+    </div>
+   
+  )
+}
+function employe(){
+  return(
+    <h1>mohd anas</h1>
+  )
+}
+
+
+
+export default App
